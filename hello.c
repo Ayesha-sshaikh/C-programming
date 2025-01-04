6acc52c45c943ce82ce9995c338bfd34e4f57c9e
Ayesha shaikh <ayesha.shaikh3404@gmail.com>
2025-01-04 00:55:13 -0800

    add paragraph

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter value of a:");
+    scanf("%d",&a);
+    printf("enter value of b:");
+    scanf("%d",&b);
+    c= a+b;
+    printf("sum of given two number is: %d",c);
+    return 0;
+}
