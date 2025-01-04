4521afffa65fb4d0e398b49144331442d3705922
Ayesha shaikh <ayesha.shaikh3404@gmail.com>
2025-01-04 01:18:16 -0800

    area of square

---
+++ .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int l,area;
+    //Area of square
+    printf("enter length of square l: ");
+    scanf("%d", &l);
+    area= l*l;
+    printf("area of square is: %d",area);
+    return 0;
+
+}
+
